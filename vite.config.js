c062927356c462bb084ea0fa0bf8cc0306bda6ec
Anirudh Kadian <coderanirudh@gmail.com>
2025-02-04 22:39:34 +0530

    deploy3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
 export default defineConfig({
-  base: "/", // Change from "./" to "/"
+  base: "/weather-app-vite/", // Change back to this
   plugins: [react()],
   build: {
     outDir: "dist",
