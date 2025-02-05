6e7d30d88c4017faf60a911beb6891b9c809d876
Anirudh Kadian <coderanirudh@gmail.com>
2025-02-05 13:25:55 +0530

    vercel json changed

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,7 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// export default defineConfig({
-//   base: "/weather-app-vite/", // Change back to this
-//   plugins: [react()],
-//   build: {
-//     outDir: "dist",
-//     rollupOptions: {
-//       output: {
-//         manualChunks(id) {
-//           if (id.includes("node_modules")) {
-//             return "vendor";
-//           }
-//         },
-//       },
-//     },
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
 export default defineConfig({
-  base: "./",
+  base: "/weather-app-vite/", // Change back to this
   plugins: [react()],
   build: {
     outDir: "dist",
@@ -35,14 +15,34 @@ export default defineConfig({
       },
     },
   },
-  // Add this to handle external scripts
-  optimizeDeps: {
-    include: ["leaflet"],
-  },
-  // Add this to resolve potential MIME type issues
-  server: {
-    fs: {
-      allow: [".."],
-    },
-  },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+
+// export default defineConfig({
+//   base: "./",
+//   plugins: [react()],
+//   build: {
+//     outDir: "dist",
+//     rollupOptions: {
+//       output: {
+//         manualChunks(id) {
+//           if (id.includes("node_modules")) {
+//             return "vendor";
+//           }
+//         },
+//       },
+//     },
+//   },
+//   // Add this to handle external scripts
+//   optimizeDeps: {
+//     include: ["leaflet"],
+//   },
+//   // Add this to resolve potential MIME type issues
+//   server: {
+//     fs: {
+//       allow: [".."],
+//     },
+//   },
+// });
